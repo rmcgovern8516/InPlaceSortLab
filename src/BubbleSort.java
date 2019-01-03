57654bbf06e8d7b85d349ef19a9f3c00f18ba70c
BT_1N3_32 <rmcgovern8516@bths.edu>
2019-01-03 08:40:39 -0500

    Version 2

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,18 +1,35 @@
 public class BubbleSort {
-    public static void swap(int[] arr, int i, int j){
+    public static void swap(int[] arr, int i, int j)
+    {
         int temp = arr[i];
         arr[i]=arr[j];
         arr[j]=temp;
     }
 
-    public static void bubbleSort(int[] arr){
-        boolean state = true;
-        while (state = true){
-            for (int i=0; i<arr.length-1; i++){
-                while (arr[i]>arr[i+1]){
-                    swap(arr,i,i+1);
+    public static void bubbleSort(int[] arr)
+    {
+        int count = 1;
+        while (count != 0)
+        {
+            count = 0;
+            for (int i=0; i<arr.length-1; i++)
+            {
+                if (arr[i] > arr[i+1])
+                {
+                        swap(arr, arr[i],arr[i+1]);
+                        count++;
                 }
             }
         }
+    }
+
+    public static int[] randIntArr(int count)
+    {
+        int[] arr = new int[count];
+        for (int i=0; i<count; i++)
+        {
+            arr[i] = (int)(Math.random()*10001);
+        }
+        return arr;
     }
 }
